46ea7f937458c99e9e44e94f9ea2de0311262271
Vikas <vikcher123@icloud.com>
2016-01-09 18:24:00 -0500

    Initial Commit

---
+++ FirstiOSApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstiOSApp
+//
+//  Created by Vikas Cheruku on 1/9/16.
+//  Copyright © 2016 XYZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
